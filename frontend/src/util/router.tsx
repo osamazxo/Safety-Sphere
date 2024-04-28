6287741fd770373a1a8537c56ba97a8da6643f95
Osama Safwat <rovendix@gmail.com>
2024-04-28 05:44:39 +0300

    initial commit

---
+++ frontend/src/util/router.tsx
@@ -0,0 +1,17 @@
+import Home from "@pages/Home";
+import Layout from "@pages/Layout";
+import { createBrowserRouter } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+    ],
+  },
+]);
+export { router };
