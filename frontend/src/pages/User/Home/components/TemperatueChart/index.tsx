daa10b9688ec8c1cb7a147f4b8a560fc7064e939
Osama Safwat <rovendix@gmail.com>
2024-05-17 08:05:16 +0300

    added react apex chart package

---
+++ frontend/src/pages/User/Home/components/TemperatueChart/index.tsx
@@ -0,0 +1,11 @@
+import { Box } from "@mui/material";
+import Grid2 from "@mui/material/Unstable_Grid2/Grid2";
+const TemperatueChart = () => {
+  return (
+    <Grid2 xs={12} md={8}>
+      <Box></Box>
+    </Grid2>
+  );
+};
+
+export default TemperatueChart;
